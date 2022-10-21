4dd1d494fd0f04974d9606c16b5fd94e8b741214
Berkaysln <berkaysln1@gmail.com>
2022-10-21 12:54:27 +0300

    project created

---
+++ dndapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dndapp
+//
+//  Created by Berkay Yaslan on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
